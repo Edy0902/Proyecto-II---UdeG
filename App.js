9eb2c232d69dcf94ab610b67b3ac95ea067bfb4e
Edgar Martinez <90947087+Edy0902@users.noreply.github.com>
2024-03-05 01:08:17 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+//App.js
+import React from 'react';
+import Calculator from './Calculadora.js';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Calculator</h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
